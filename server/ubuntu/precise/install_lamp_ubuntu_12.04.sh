cbbefdd74993d01c9a6cd468d58420d0c556f2c4
Charles SANQUER <charles.sanquer@spyrit.net>
2012-08-06 16:40:41 +0200

    add postgresql packages

--- server/ubuntu/precise/install_lamp_ubuntu_12.04.sh
+++ server/ubuntu/precise/install_lamp_ubuntu_12.04.sh
@@ -4,8 +4,9 @@ sudo apt-get -y update
 sudo apt-get -y upgrade 
 
 sudo apt-get -y install mysql-client mysql-server
+sudo apt-get -y install postgresql 
 sudo apt-get -y install apache2-mpm-worker apache2-utils libapache2-mod-fcgid apache2-suexec-custom
-sudo apt-get -y install php5 php-apc php-pear php5-cgi php5-cli php5-fpm php5-dev php5-curl php5-gd php5-imagick php5-imap php5-intl php5-mcrypt php5-mysql php5-sqlite php5-xdebug php5-xmlrpc php5-xsl
+sudo apt-get -y install php5 php-apc php-pear php5-cgi php5-cli php5-fpm php5-dev php5-curl php5-gd php5-imagick php5-imap php5-intl php5-mcrypt php5-mysql php5-sqlite php5-xdebug php5-xmlrpc php5-xsl php5-pgsql 
 
 sudo cp -R assets/etc/apache2/* /etc/apache2/
 sudo cp -R assets/var/www/* /var/www/
@@ -16,4 +17,4 @@ sudo sed -i '/ServerRoot \"\/etc\/apache2\"/ aServerName workstation.local' /etc
 sudo a2enmod actions alias fcgid headers vhost_alias suexec  rewrite 
 sudo service apache2 restart
 
-sudo apt-get -y install phpmyadmin
+sudo apt-get -y install phpmyadmin phppgadmin pgadmin3
