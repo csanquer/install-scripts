78b2fdb5ec4d208b4f3d09bb415fc7f79c4e0b48
Charles SANQUER <charles.sanquer@spyrit.net>
2012-08-07 00:36:32 +0200

    add postfix and dev libraries

--- server/ubuntu/precise/install_lamp_ubuntu_12.04.sh
+++ server/ubuntu/precise/install_lamp_ubuntu_12.04.sh
@@ -3,10 +3,10 @@
 sudo apt-get -y update 
 sudo apt-get -y upgrade 
 
-sudo apt-get -y install mysql-client mysql-server
-sudo apt-get -y install postgresql 
-sudo apt-get -y install apache2-mpm-worker apache2-utils libapache2-mod-fcgid apache2-suexec-custom
-sudo apt-get -y install php5 php-apc php-pear php5-cgi php5-cli php5-fpm php5-dev php5-curl php5-gd php5-imagick php5-imap php5-intl php5-mcrypt php5-mysql php5-sqlite php5-xdebug php5-xmlrpc php5-xsl php5-pgsql 
+sudo apt-get -y install mysql-client mysql-server libmysqlclient-dev
+sudo apt-get -y install postgresql libpq-dev
+sudo apt-get -y install apache2-mpm-worker apache2-utils libapache2-mod-fcgid apache2-suexec-custom apache2-threaded-dev curl postfix
+sudo apt-get -y install php5 php-apc php-pear php5-cgi php5-cli php5-fpm php5-dev php5-curl php5-gd php5-imagick php5-imap php5-intl php5-mcrypt php5-mysql php5-sqlite php5-xdebug php5-xmlrpc php5-xsl php5-pgsql imagemagick libmagickcore-dev libmagickwand-dev
 
 # Configure Apache and FCGID
 sudo cp -R assets/etc/apache2/* /etc/apache2/
