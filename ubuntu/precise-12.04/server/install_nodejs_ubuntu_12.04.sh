011d9870d82b62db915b15039f6e5210bdd42504
Charles SANQUER <charles.sanquer@gmail.com>
2012-09-02 10:02:33 +0200

    add node.js and less css preprocessor install script

---
+++ ubuntu/precise-12.04/server/install_nodejs_ubuntu_12.04.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# install node.js (global system)
+
+sudo apt-get install -y build-essential git python libssl-dev -y
+
+git clone git://github.com/joyent/node.git
+cd node
+git checkout v0.9.1
+
+./configure
+make
+sudo make install
+
+sudo npm install less recess -g
