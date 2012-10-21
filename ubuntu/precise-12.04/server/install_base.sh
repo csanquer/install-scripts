89e9084c16f1337edb3f73cd3172a657ea88bb52
Charles SANQUER <charles.sanquer@gmail.com>
2012-10-21 16:45:47 +0200

    add server base script install

---
+++ ubuntu/precise-12.04/server/install_base.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get -y update 
+sudo apt-get -y upgrade 
+
+sudo apt-get -y install openssh-server openssh-client openssh-blacklist openssh-blacklist-extra nano kate openssl openssl-blacklist openssl-blacklist-extra bash-completion zsh zsh-doc vim vim-doc emacs trash-cli tree mc ack-grep manpages-fr manpages-fr-dev manpages-fr-extra build-essential bison p7zip p7zip-full p7zip-rar lzma unrar rar curl ncftp lftp rsync subversion subversion-tools git git-cola git-core git-svn git-email git-doc mercurial mercurial-git hgsvn cvs cvs2svn screen tmux tofrodos diffutils colordiff patch dos2unix openjdk-7-jdk clamav clamav-freshclam
