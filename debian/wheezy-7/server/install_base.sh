d281b8f2f1901295acd4cf519643363764ef983a
Charles Sanquer <charles.sanquer@gmail.com>
2013-07-06 17:00:43 +0200

    add kdesdk scripts package

--- debian/wheezy-7/server/install_base.sh
+++ debian/wheezy-7/server/install_base.sh
@@ -20,7 +20,7 @@ sudo cp -R assets/etc/skel /etc/
 sudo $aptbin -y update 
 sudo $aptbin -y dist-upgrade 
 
-sudo $aptbin -y install openssh-server openssh-client openssh-blacklist openssh-blacklist-extra nano openssl openssl-blacklist openssl-blacklist-extra bash-completion zsh zsh-doc vim vim-doc emacs  tree mc acl ack-grep manpages-fr manpages-fr-dev manpages-fr-extra build-essential bison p7zip p7zip-full p7zip-rar lzma unrar rar curl ncftp lftp rsync subversion subversion-tools git git-core git-svn git-email git-doc mercurial mercurial-git hgsvn cvs cvs2svn screen tmux tofrodos diffutils colordiff patch dos2unix clamav clamav-freshclam openjdk-7-jdk python-software-properties ntpdate fail2ban ufw locales tzdata ntpdate
+sudo $aptbin -y install openssh-server openssh-client openssh-blacklist openssh-blacklist-extra nano openssl openssl-blacklist openssl-blacklist-extra bash-completion zsh zsh-doc vim vim-doc emacs  tree mc acl ack-grep manpages-fr manpages-fr-dev manpages-fr-extra build-essential bison p7zip p7zip-full p7zip-rar lzma unrar rar curl ncftp lftp rsync kdesdk-scripts subversion subversion-tools git git-core git-svn git-email git-doc mercurial mercurial-git hgsvn cvs cvs2svn screen tmux tofrodos diffutils colordiff patch dos2unix clamav clamav-freshclam openjdk-7-jdk python-software-properties ntpdate fail2ban ufw locales tzdata ntpdate
 
 sudo dpkg-reconfigure tzdata
 sudo dpkg-reconfigure locales
