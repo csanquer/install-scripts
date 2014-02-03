7a77ba9832f36f63f26fb5954e30f08ef0f77232
Charles Sanquer <charles.sanquer@gmail.com>
2014-02-03 17:07:14 +0100

    update nodejs version

--- ubuntu/saucy-13.10/server/install_nodejs.sh
+++ ubuntu/saucy-13.10/server/install_nodejs.sh
@@ -9,7 +9,7 @@ sudo $aptbin install -y build-essential git python libssl-dev
 
 git clone https://github.com/joyent/node.git
 cd node
-git checkout v0.11.9
+git checkout v0.11.11
 
 ./configure
 make
