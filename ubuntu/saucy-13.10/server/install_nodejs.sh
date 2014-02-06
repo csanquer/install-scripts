35077022a6cf9c81a54741ac95570e5ca988e0dc
Charles Sanquer <charles.sanquer@gmail.com>
2014-02-06 17:27:37 +0100

    improve node install

--- ubuntu/saucy-13.10/server/install_nodejs.sh
+++ ubuntu/saucy-13.10/server/install_nodejs.sh
@@ -2,13 +2,33 @@
 
 # install node.js (global system)
 
-aptbin=aptitude
-#aptbin=apt-get
+PS3='Package Manager to use : '
+options=("apt-get" "aptitude")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "apt-get")
+            aptbin=apt-get
+            break;;
+        "aptitude")
+            aptbin=aptitude
+            break;;
+        *)
+        echo invalid option
+        break;;
+    esac
+done
 
 sudo $aptbin install -y build-essential git python libssl-dev 
 
-git clone https://github.com/joyent/node.git
-cd node
+if [ -d 'node' ]; then
+    cd node
+    git fetch
+else
+    git clone https://github.com/joyent/node.git
+    cd node
+fi
+
 git checkout v0.11.11
 
 ./configure
