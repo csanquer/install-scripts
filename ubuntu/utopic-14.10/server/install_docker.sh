51d70128f868dbb8c0af045dd705229fbbdc95cf
Charles Sanquer <csanquer@csanquer-XPS-13-kilix>
2015-06-28 12:35:00 +0200

    improve docker install script

--- ubuntu/utopic-14.10/server/install_docker.sh
+++ ubuntu/utopic-14.10/server/install_docker.sh
@@ -2,6 +2,9 @@
 
 # install docker
 
+dockerComposeVersion=1.3.1
+dockerMachineVersion=0.3.0
+
 PS3='Package Manager to use : '
 options=("apt-get" "aptitude")
 select opt in "${options[@]}"
@@ -40,16 +43,30 @@ sudo usermod -a -G docker ${USER}
 
 sudo service docker restart
 
-echo "docker version"
 sudo docker --version
 
 # install docker compose
-curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > docker-compose
+curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` > docker-compose
 sudo mv docker-compose /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
 # docker compose bash completion
-curl -L https://raw.githubusercontent.com/docker/compose/1.2.0/contrib/completion/bash/docker-compose > docker-compose-completion
+curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose --version | awk 'NR==1{print $NF}')/contrib/completion/bash/docker-compose > docker-compose-completion
 sudo mv docker-compose-completion /etc/bash_completion.d/docker-compose
 
-echo "docker-compose version"
-sudo docker-compose --version
+# docker compose zsh completion
+mkdir -p ~/.zsh/completion
+curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose --version | awk 'NR==1{print $NF}')/contrib/completion/zsh/_docker-compose > ~/.zsh/completion/_docker-compose
+
+echo 'fpath=(~/.zsh/completion $fpath)' >> ~/.zshrc
+echo 'autoload -Uz compinit && compinit -i' >> ~/.zshrc
+exec $SHELL -l
+
+# install docker machine
+curl -L https://github.com/docker/machine/releases/download/$dockerMachineVersion/docker-machine_`uname -s`-`uname -m` > docker-machine
+sudo mv docker-machine /usr/local/bin/docker-machine
+sudo chmod +x /usr/local/bin/docker-machine
+
+docker-machine -v
