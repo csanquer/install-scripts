ae4f5ab6f04c335be06c59609c894beace1a4836
Charles Sanquer <charles.sanquer@gmail.com>
2016-05-09 07:56:35 +0200

    begin ansible refactoring

--- README.md
+++
@@ -1,4 +0,0 @@
-install-scripts [![Project Status](http://stillmaintained.com/csanquer/install-scripts.png)](http://stillmaintained.com/csanquer/install-scripts)
-===============
-
-Linux desktop workstation and server installation scripts
---
+++ install.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+
+if [ ! -x "$(command -v lsb_release)" ]; then
+    echo 'please install lsb release package'
+    if [ -x "$(command -v apt-get)" ]; then
+        sudo apt-get install -y lsb-release
+    #elif [ -x "$(command -v yum)" ]; then
+    #    sudo yum install -y lsb-release
+    fi
+fi
+
+user=`whoami`
+
+distId=`lsb_release -si`
+distRelease=`lsb_release -sr`
+distCodename=`lsb_release -sc`
+
+if [ "$distId" = 'Debian' -o "$distId" = 'Ubuntu' ]; then
+    sudo apt-get install -y build-essential libffi-dev libssl-dev python python-dev python-setuptools git
+    sudo easy_install pip
+    sudo pip install -U ansible
+fi
+
+ansible-galaxy install -r roles.txt -p roles
+
+ansible-playbook main.yml --ask-become-pass -u $user
