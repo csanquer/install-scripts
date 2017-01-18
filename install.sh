6698435d22cc289fd1a26fe46895a3c612f2dff4
Charles Sanquer <charles.sanquer@gmail.com>
2017-01-18 17:07:21 +0100

    improve sudo password prompt

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #/bin/bash
 
+echo -n  "SUDO Password: "
+stty -echo
+read sudopassword
+stty echo
+echo
+
 if [ ! -x "$(command -v lsb_release)" ]; then
     if [ -x "$(command -v apt-get)" ]; then
         echo 'installing lsb release'
-        sudo apt-get install -y lsb-release
+        echo $sudopassword | sudo -S  apt-get install -y lsb-release
     # elif [ -x "$(command -v yum)" ]; then
     #    sudo yum install -y redhat-lsb
     fi
@@ -15,25 +21,27 @@ distId=`lsb_release -si`
 distRelease=`lsb_release -sr`
 distCodename=`lsb_release -sc`
 
+
+
 if [ "$distId" = 'Debian' -o "$distId" = 'Ubuntu' ]; then
     if [ ! -x "$(command -v pip)" ]; then
         echo "installing Python and build dependencies ..."
-        sudo apt-get install -y -q build-essential libffi-dev libssl-dev python python-dev python-setuptools git
-        sudo easy_install -q pip
+        echo $sudopassword | sudo -S apt-get install -y -q build-essential libffi-dev libssl-dev python python-dev python-setuptools git
+        echo $sudopassword | sudo -S  easy_install -q pip
     fi
     echo "installing Ansible ..."
-    sudo pip install -q -U ansible
+    echo $sudopassword | sudo -S  pip install -q -U ansible
 fi
 
 echo "installing ansible galaxy roles"
 ansible-galaxy install -r roles.txt -p roles
 
-playbook_options="-u $user --ask-become-pass $@"
+playbook_options="-u $user $@"
 
 if [ -f "vault/secrets.yml" ];then
     playbook_options="$playbook_options --ask-vault-pass"
 fi
 
 echo "running ansible playbook ..."
-echo "command : ansible-playbook main.yml $playbook_options"
-ansible-playbook main.yml $playbook_options
+echo "command : ansible-playbook main.yml --ask-become-pass $playbook_options"
+ansible-playbook main.yml -e "ansible_become_pass=$sudopassword" $playbook_options
