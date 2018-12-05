0a625a7f66b39037fda56ec3c7f2905ec96bca71
Charles Sanquer <charles.sanquer@gmail.com>
2018-12-05 13:12:15 +0100

    update all submodules for ansible 2.7

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ if [ "$distId" = 'Debian' -o "$distId" = 'Ubuntu' ]; then
 fi
 
 echo "installing ansible galaxy roles"
-ansible-galaxy install -r roles.txt -p roles
+ansible-galaxy install -r roles.yml -p roles
 
 playbook_options="-u $user $@"
 
