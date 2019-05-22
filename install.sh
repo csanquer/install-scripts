43a20557d0782c6c61cb85577004228a541291cc
Charles Sanquer <charles.sanquer@cubyn.com>
2019-05-22 10:12:50 +0200

    add new ansible deps : jmespath

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ if [ "$distId" = 'Debian' -o "$distId" = 'Ubuntu' ]; then
     fi
     if [ ! -x "$(command -v ansible)" ]; then
         echo "installing Ansible ..."
-        echo "$sudopassword\n" | sudo -S -p '' pip install -q -U ansible
+        echo "$sudopassword\n" | sudo -S -p '' pip install -q -U ansible jmespath
     fi
 fi
 
